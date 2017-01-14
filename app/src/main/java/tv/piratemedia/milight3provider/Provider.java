3d9c70f5fe6097314ca21b2af501972bc388f804
eliotstocker <eliot@piratemedia.tv>
2017-01-14 23:55:36 +0000

    Initial commit

---
+++ app/src/main/java/tv/piratemedia/milight3provider/Provider.java
@@ -0,0 +1,244 @@
+package tv.piratemedia.milight3provider;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import java.io.UnsupportedEncodingException;
+
+import tv.piratemedia.lightcontroler.api.ControlProviderReciever;
+import tv.piratemedia.lightcontroler.api.appPermission;
+
+/**
+ * Created by eliotstocker on 14/01/2017.
+ */
+
+public class Provider extends ControlProviderReciever {
+    private static appPermission[] ap = {new appPermission("tv.piratemedia.lightcontroler", -448508482)};
+
+
+    private UDPConnection connection = null;
+    private Handler handler = null;
+    private String DiscoveredIP = null;
+    private String DiscoveredMac = null;
+    private SharedPreferences prefs;
+    private byte[] deviceSig = null;
+
+    private int SequenceNumberIndex = 0;
+
+    public Provider() {
+        super(ap);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        if(DiscoveredIP == null) {
+            prefs.getString("pref_light_controller_ip", null);
+        }
+        if(DiscoveredMac == null) {
+            prefs.getString("pref_light_controller_mac", null);
+        }
+
+        if(handler == null) {
+            handler = new Handler() {
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+                    switch(msg.what) {
+                        case UDPConnection.DISCOVERED_DEVICE:
+                            String[] ipMac = (String[])msg.obj;
+                            DiscoveredIP = ipMac[0];
+                            DiscoveredMac = ipMac[2];
+
+                            prefs.edit().putString("pref_light_controller_ip", DiscoveredIP).commit();
+                            prefs.edit().putString("pref_light_controller_mac", DiscoveredMac).commit();
+                            Log.d("Retrieved Device Info", "IP: "+DiscoveredIP+", MAC: "+DiscoveredMac);
+
+                            getSessionID();
+                            break;
+                        case UDPConnection.SIGNATURE:
+                            deviceSig = (byte[])msg.obj;
+                            Log.d("Retrieved Signature", "Sig: "+deviceSig);
+                            break;
+                    }
+                }
+            };
+        }
+        if(connection == null) {
+            connection = new UDPConnection(context, handler);
+        }
+        super.onReceive(context, intent);
+    }
+
+    public void getSessionID() {
+        byte[] bytes = new byte[]{
+                0x20,
+                0x00,
+                0x00,
+                0x00,
+                0x16,
+                0x02,
+                0x62,
+                0x3A,
+                (byte)0xD5,
+                (byte)0xED,
+                (byte)0xA3,
+                0x01,
+                (byte)0xAE,
+                0x08,
+                0x2D,
+                0x46,
+                0x61,
+                0x41,
+                (byte)0xA7,
+                (byte)0xF6,
+                (byte)0xDC,
+                (byte)0xAF,
+                (byte)0xD3,
+                (byte)0xE6,
+                0x00,
+                0x00,
+                0x1E
+        };
+        connection.sendAdminMessage(bytes, true);
+    }
+
+    public byte[] constructCommand(byte[] command, byte zone) {
+        byte[] header = new byte[] {
+                (byte)0x80,
+                0x00,
+                0x00,
+                0x00,
+                0x11,
+                deviceSig[0],
+                deviceSig[1],
+                0x00,
+                (byte)SequenceNumberIndex,
+                0x00
+        };
+
+        byte[] zoneInfo = new byte[] {
+                zone,
+                0x00
+        };
+
+        int checksum = 0;
+        for (byte aCommand : command) {
+            checksum += aCommand;
+        }
+        for (byte aZoneInfo : zoneInfo) {
+            checksum += aZoneInfo;
+        }
+
+        byte[] c = new byte[header.length + command.length + zoneInfo.length + 1];
+
+        System.arraycopy(header, 0, c, 0, header.length);
+        System.arraycopy(command, 0, c, header.length, command.length);
+        System.arraycopy(zoneInfo, 0, c, command.length, zoneInfo.length);
+        c[c.length -1] = (byte)checksum;
+
+        return c;
+    }
+
+    @Override
+    public void onDiscovery(Context context) {
+        Log.d("Provider", "Starting Discovery of Provider 3.0");
+        try {
+            connection.sendAdminMessage("HF-A11ASSISTHREAD".getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLightsOn(int Type, int Zone, Context context) {
+        byte zoneByte = 0x00;
+        switch(Zone) {
+            case 0: zoneByte = 0x00; break;
+            case 1: zoneByte = 0x01; break;
+            case 2: zoneByte = 0x02; break;
+            case 3: zoneByte = 0x03; break;
+            case 4: zoneByte = 0x04; break;
+        }
+        byte[] data = constructCommand(new byte[] {
+                0x31, 0x00,
+                0x00, 0x08,
+                0x04, 0x01,
+                0x00, 0x00,
+                0x00
+        }, zoneByte);
+
+        connection.sendMessage(data);
+        Log.d("Provider", "Send On Command: "+data);
+    }
+
+    @Override
+    public void onLightsOff(int Type, int Zone, Context context) {
+
+    }
+
+    @Override
+    public void onGlobalOn(Context context) {
+
+    }
+
+    @Override
+    public void onGlobalOff(Context context) {
+
+    }
+
+    @Override
+    public void onSetBrightness(int Type, int Zone, float Brightness, Context context) {
+
+    }
+
+    @Override
+    public void onIncreaseBrightness(int Type, int Zone, Context context) {
+
+    }
+
+    @Override
+    public void onDecreaseBrightness(int Type, int Zone, Context context) {
+
+    }
+
+    @Override
+    public void onSetColor(int Zone, int color, Context context) {
+
+    }
+
+    @Override
+    public void onSetTemperature(int Type, int Zone, float Temp, Context context) {
+
+    }
+
+    @Override
+    public void onIncreaseTemperature(int Type, int Zone, Context context) {
+
+    }
+
+    @Override
+    public void onDecreaseTemperature(int Type, int Zone, Context context) {
+
+    }
+
+    @Override
+    public void onSetNight(int Type, int Zone, Context context) {
+
+    }
+
+    @Override
+    public void onSetFull(int Type, int Zone, Context context) {
+
+    }
+
+    @Override
+    public void onSetWhite(int Zone, Context context) {
+
+    }
+}
---
+++ app/src/main/java/tv/piratemedia/milight3provider/UDPConnection.java
@@ -0,0 +1,224 @@
+/*
+*    Light Controller, to Control wifi LED Lighting
+*    Copyright (C) 2014  Eliot Stocker
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package tv.piratemedia.milight3provider;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+
+public class UDPConnection {
+    public static final int DISCOVERED_DEVICE = 10;
+    public static final int COMMAND_SUCCESS = 20;
+    public static final int LIST_WIFI_NETWORKS = 30;
+    public static final int SIGNATURE = 40;
+
+    public static String CONTROLLERIP = "";
+    public static int CONTROLLERPORT = 0;
+    public static int CONTROLLERADMINPORT = 48899;
+    private utils Utils;
+    private UDP_Server server = null;
+    private SharedPreferences prefs;
+    private static Context mCtx;
+    private static Handler mHandler;
+    private String NetworkBroadCast;
+
+    private boolean onlineMode = false;
+
+    public UDPConnection(Context context, Handler handler) {
+        mCtx = context;
+        mHandler = handler;
+        Utils = new utils(context);
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        NetworkBroadCast = "192.168.0.255";
+        try {
+            NetworkBroadCast = Utils.getWifiIP(utils.BROADCAST_ADDRESS);
+        } catch (ConnectionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setOnlineMode(boolean online) {
+        onlineMode = online;
+    }
+
+    public void sendMessage(final byte[] Bytes) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if(!onlineMode) {
+                    CONTROLLERIP = prefs.getString("pref_light_controller_ip", NetworkBroadCast);
+                    CONTROLLERPORT = Integer.parseInt(prefs.getString("pref_light_controller_port", "5987"));
+                    try {
+                        DatagramSocket s = new DatagramSocket();
+                        InetAddress controller = InetAddress.getByName(CONTROLLERIP);
+                        DatagramPacket p = new DatagramPacket(Bytes, 3, controller, CONTROLLERPORT);
+                        s.send(p);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    //send message in online mode;
+                }
+            }
+        }).start();
+    }
+
+    public void sendAdminMessage(byte[] Bytes) {
+        sendAdminMessage(Bytes, false);
+    }
+
+    public void sendAdminMessage(final byte[] Bytes, final Boolean Device) {
+        if(server == null) {
+            server = new UDP_Server();
+            server.runUdpServer();
+        } else if(!server.Server_aktiv) {
+            server.runUdpServer();
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String NetworkBroadCast = null;
+                if (Device) {
+                    CONTROLLERIP = prefs.getString("pref_light_controller_ip", "192.168.0.255");
+                    NetworkBroadCast = CONTROLLERIP;
+                } else {
+                    try {
+                        NetworkBroadCast = Utils.getWifiIP(utils.BROADCAST_ADDRESS);
+                    } catch (ConnectionException e) {
+                        e.printStackTrace();
+                        return;
+                    }
+                }
+                try {
+                    DatagramSocket s = new DatagramSocket();
+                    InetAddress controller = InetAddress.getByName(NetworkBroadCast);
+                    DatagramPacket p = new DatagramPacket(Bytes, Bytes.length, controller, CONTROLLERADMINPORT);
+                    s.setBroadcast(true);
+                    s.send(p);
+                } catch(IOException e) {
+
+                }
+            }
+        }).start();
+    }
+
+    public void destroyUDPC() {
+        Log.d("controller", "destroy");
+        if(server != null) {
+            server.stop_UDP_Server();
+        }
+    }
+
+    class UDP_Server {
+        private AsyncTask<Void, Void, Void> async;
+        public boolean Server_aktiv = true;
+
+        @SuppressLint("NewApi")
+        public void runUdpServer() {
+            Server_aktiv = true;
+            async = new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... params) {
+                    byte[] lMsg = new byte[1024];
+                    DatagramPacket dp = new DatagramPacket(lMsg, lMsg.length);
+                    DatagramSocket ds = null;
+
+                    try {
+                        ds = new DatagramSocket(UDPConnection.CONTROLLERADMINPORT);
+                        ds.setSoTimeout(1000);
+                        while (Server_aktiv) {
+                            try {
+                                ds.receive(dp);
+                                byte[] data = dp.getData();
+                                if(data[0] == 0x28 && data[1] == 0x00 && data[2] == 0x00
+                                        && data[4] == 0x00 && data[5] == 0x11
+                                        && data[6] == 0x00 && data[7] == 0x02) {
+                                    byte[] sig = new byte[] {data[19], data[20]};
+                                    Message m = new Message();
+                                    m.what = SIGNATURE;
+                                    m.obj = sig;
+                                    mHandler.sendMessage(m);
+                                    Server_aktiv = false;
+                                }
+                                String Data = new String(data);
+                                Log.d("Received Data", Data);
+                                if(Data.startsWith("+ok")) {
+                                    if(Data.startsWith("+ok=")) {
+                                        Message m = new Message();
+                                        m.what = LIST_WIFI_NETWORKS;
+                                        m.obj = Data;
+                                        mHandler.sendMessage(m);
+                                        Server_aktiv = false;
+                                    } else {
+                                        Message m = new Message();
+                                        m.what = COMMAND_SUCCESS;
+                                        mHandler.sendMessage(m);
+                                        Server_aktiv = false;
+                                    }
+                                } else {
+                                    String[] parts = Data.split(",");
+                                    if (parts.length > 1) {
+                                        if (Utils.validIP(parts[0]) && Utils.validMac(parts[1])) {
+                                            Message m = new Message();
+                                            m.what = DISCOVERED_DEVICE;
+                                            m.obj = parts;
+                                            mHandler.sendMessage(m);
+                                            Server_aktiv = false;
+                                        }
+                                    }
+                                }
+                            } catch(SocketTimeoutException e) {
+                                //no problem
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (ds != null) {
+                            ds.close();
+                        }
+                    }
+
+                    return null;
+                }
+            };
+
+            if (Build.VERSION.SDK_INT >= 11)
+                async.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+            else async.execute();
+        }
+
+        public void stop_UDP_Server() {
+            Server_aktiv = false;
+        }
+    }
+}
